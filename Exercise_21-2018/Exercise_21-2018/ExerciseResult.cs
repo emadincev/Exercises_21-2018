e451cdab84c69049e82ae9a157e21ffbb06e5dcc
Emilija Dincev <ema.dincev@gmail.com>
2020-11-09 16:47:54 +0100

    Komit 3 - Kreirana klasa ExerciseResult

---
+++ Exercise_21-2018/Exercise_21-2018/ExerciseResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.Pkcs;
+using System.Text;
+
+namespace Exercise_21_2018
+{
+    class ExerciseResult
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string StudentIndex { get; set; }
+        public int Points { get; set; }
+    }
+}
