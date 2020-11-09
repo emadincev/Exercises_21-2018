fa0cf5a86926f489872fdeca7870347318371a7a
Emilija Dincev <ema.dincev@gmail.com>
2020-11-09 16:43:09 +0100

    Komit 2 - Povezivanje sa bazom podataka

--- Exercise_21-2018/Exercise_21-2018/Form1.cs
+++ Exercise_21-2018/Exercise_21-2018/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace Exercise_21_2018
 {
     public partial class Form1 : Form
     {
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FacultyDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +21,12 @@ namespace Exercise_21_2018
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            SqlConnection sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString = connectionString;
 
+            sqlConnection.Open();
+
+            sqlConnection.Close();
         }
     }
 }
