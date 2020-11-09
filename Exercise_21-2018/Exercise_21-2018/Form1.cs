d7846730bb178a64f6b3fb2879bf4c4b25b8ad70
Emilija Dincev <ema.dincev@gmail.com>
2020-11-09 17:10:42 +0100

    Komit 5 - Ispisivanje podataka o studentima u ListBoxExerciseResults

--- Exercise_21-2018/Exercise_21-2018/Form1.cs
+++ Exercise_21-2018/Exercise_21-2018/Form1.cs
@@ -32,6 +32,23 @@ namespace Exercise_21_2018
             sqlCommand.Connection = sqlConnection;
             sqlCommand.CommandText = "SELECT * FROM ExerciseResults";
 
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            while (sqlDataReader.Read())
+            {
+                ExerciseResult student = new ExerciseResult();
+                student.Id = sqlDataReader.GetInt32(0);
+                student.StudentName = sqlDataReader.GetString(1);
+                student.StudentIndex = sqlDataReader.GetString(2);
+                student.Points = sqlDataReader.GetInt32(3);
+                students.Add(student);
+            }
+
+            foreach(ExerciseResult s in students)
+            {
+                listBoxExerciseResults.Items.Add(s.Id + ". " + s.StudentName + " " + s.StudentIndex + "  = " + s.Points);
+            }
+
             sqlConnection.Close();
         }
     }
